3b3d913b01b0ebfafc3ea1eea679b3f0bd77c6dc
Andreea Topita <andreea-loredana.topita@student.tuiasi.ro>
2025-05-08 22:58:47 +0300

    update complet lab10

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
 
+
+//importam lista de intrebari din fisierul json
+// fisierul json contine un array de obiecte cu intrebari, variante si raspuns corect
+const fs = require('fs').promises;
+const path = require('path');
+
+
+
 const app = express();
 const port = 6789;
 
@@ -22,6 +30,16 @@ app.use(bodyParser.json());
 // utilizarea unui algoritm de deep parsing care suportă obiecte în obiecte
 app.use(bodyParser.urlencoded({ extended: true }));
 
+
+
+//functie asicrona de incarcare a fisierului json
+async function loadIntrebari() {
+    const filePath = path.join(__dirname, 'intrebari.json');
+    const data = await fs.readFile(filePath, 'utf8');
+    return JSON.parse(data);
+}
+
+
 // la accesarea din browser adresei http://localhost:6789/ se va returna textul 'Hello World'
 // proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
@@ -30,24 +48,52 @@ app.get('/', (req, res) => {
 });
 
 // la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
-app.get('/chestionar', (req, res) => {
-  const listaIntrebari = [
-    {
-      intrebare: 'Întrebarea 1',
-      variante: ['varianta 1', 'varianta 2', 'varianta 3', 'varianta 4'],
-      corect: 0
-    },
-    // ...
-  ];
-  // în fișierul views/chestionar.ejs este accesibilă variabila 'intrebari'
-  // care conține vectorul de întrebări
-  res.render('chestionar', { intrebari: listaIntrebari });
+app.get('/chestionar',async  (req, res) => {
+    try {
+        const intrebari = await loadIntrebari();
+        //apelam functia de incarcare a fisierului json
+
+        // în fișierul views/chestionar.ejs este accesibilă variabila 'intrebari'
+        // care conține vectorul de întrebări
+        res.render('chestionar', {
+            titlu: 'Chestionar Documente',
+            intrebari
+        });
+    } catch (err) {
+        console.error('Eroare la citirea întrebărilor:', err);
+        res.status(500).send('A apărut o eroare pe server.');
+      }
 });
 
-app.post('/rezultat-chestionar', (req, res) => {
-  console.log(req.body);
-  res.send('formular: ' + JSON.stringify(req.body));
-});
+
+app.post('/rezultat-chestionar', async (req, res) => {
+    try {
+        const intrebari = await loadIntrebari();
+
+        const raspunsuri = req.body; //toate raspunsurile
+        let cnt = 0; // contor
+    
+        //parcurg fiecare item si pozitia lui in listaIntrebari
+        //verific daca raspunsul utilizatorului este corect
+        intrebari.forEach(item  => {
+            const ales = raspunsuri[item.intrebare];
+
+            if (ales === item.variante[item.corect]) {
+                cnt++;
+            }
+        });
+
+        //trimitem catre pagina rezultat-chestionar.ejs nr de rasp corecte si total 
+        res.render('rezultat-chestionar', {
+        titlu: 'Rezultatul chestionarului',
+        nrRaspCorecte: cnt,
+        totalIntrebari: intrebari.length
+        });
+    } catch (err) {
+        console.error('Eroare la procesarea rezultatelor:', err);
+        res.status(500).send('A apărut o eroare pe server.');
+    }
+  });
 
 // pornește serverul pe portul specificat
 app.listen(port, () => {
