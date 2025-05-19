94f65b573608c9928aa95e09fb316c90afdb5335
Andreea Topita <andreea-loredana.topita@student.tuiasi.ro>
2025-05-19 19:59:35 +0300

    lab 11

--- app.js
+++ app.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const bodyParser = require('body-parser');
-
+const cookieParser=require('cookie-parser');
 
 //importam lista de intrebari din fisierul json
 // fisierul json contine un array de obiecte cu intrebari, variante si raspuns corect
 const fs = require('fs').promises;
 const path = require('path');
 
+const session = require('express-session');
 
 
 const app = express();
 const port = 6789;
+
+app.use(cookieParser());   
+
 
 // directorul 'views' va conține fișierele .ejs (html + js executat la server)
 app.set('view engine', 'ejs');
@@ -32,6 +36,31 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
+
+// middleware pentru gestionarea sesiunilor
+//middleware-ul va salva datele sesiunii in memorie
+//middleware = functie care se executa inainte de a ajunge la ruta
+app.use(session({
+  secret: 'secret-key', // cheia secreta pentru criptarea sesiunii
+  resave: false,
+  saveUninitialized: false,
+  cookie: { maxAge: 24 * 60 * 60 * 1000 } // sesiune valabilă 1 zi
+}));
+
+ //disponibil in toate view-urile ca variabila "user"
+app.use((req, res, next) => {
+  res.locals.user = req.session.user;
+  next();
+});
+
+
+
+async function loadUtilizatori() {
+  const filePath = path.join(__dirname, 'utilizatori.json');
+  const data = await fs.readFile(filePath, 'utf8');
+  return JSON.parse(data);
+}
+
 //functie asicrona de incarcare a fisierului json
 async function loadIntrebari() {
     const filePath = path.join(__dirname, 'intrebari.json');
@@ -43,9 +72,8 @@ async function loadIntrebari() {
 // la accesarea din browser adresei http://localhost:6789/ se va returna textul 'Hello World'
 // proprietățile obiectului Request - req - https://expressjs.com/en/api.html#req
 // proprietățile obiectului Response - res - https://expressjs.com/en/api.html#res
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+
+
 
 // la accesarea din browser adresei http://localhost:6789/chestionar se va apela funcția specificată
 app.get('/chestionar',async  (req, res) => {
@@ -94,6 +122,64 @@ app.post('/rezultat-chestionar', async (req, res) => {
         res.status(500).send('A apărut o eroare pe server.');
     }
   });
+
+//index.ejs
+app.get('/', (req, res) => {
+    //accesare cookie utilizator
+  //daca utilizatorul este logat, cookie-ul va contine numele lui
+    //daca nu este logat, cookie-ul va fi gol
+  const utilizator = req.cookies.utilizator;   
+  res.render('index', {
+
+    utilizator
+  });
+});
+
+
+//autentificare.ejs
+//accesare cookie mesajEroare
+//daca cookie-ul este setat, se va afisa mesajul de eroare
+app.get('/autentificare', (req, res) => {
+
+    const mesajEroare = req.session.mesajEroare;
+    req.session.mesajEroare = null;
+
+    res.render('autentificare', { mesajEroare });
+});
+
+//verificare-autentificare post
+app.post('/verificare-autentificare', async (req, res) => {
+  const { user: username, pass: password } = req.body;
+  const utilizatori = await loadUtilizatori();
+
+  // caută utilizatorul în JSON
+  const usr = utilizatori.find(u =>
+    u.username === username && u.password === password
+  );
+
+  if (usr) {
+    // 1. „Destructurăm” usr scoțând parola
+    const { parola, ...secureProps } = usr;
+
+    // 2. Salvăm în sesiune tot ce vrei fără parola
+    req.session.user = secureProps;
+
+    return res.redirect('/');
+    } else {
+        // autentificare FAIL → pune mesaj în sesiune și redirecționează
+        req.session.mesajEroare = '❌ User sau parolă invalidă!!!';
+        return res.redirect('/autentificare');
+  }
+});
+
+
+
+
+app.get('/deconectare', (req, res) => {
+    req.session.destroy(err => {
+    res.redirect('/');
+  });
+});
 
 // pornește serverul pe portul specificat
 app.listen(port, () => {
